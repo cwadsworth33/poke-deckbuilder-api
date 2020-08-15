9d4e4e87ccd485da8491024fa1ff53be6e1eabe1
cwadsworth33 <cwadsrun33@gmail.com>
2020-08-15 15:24:04 -0400

    initial commit with spring boot skeleton

---
+++ src/main/java/com/example/pokemon_deckbuilder/PokemonDeckbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemon_deckbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonDeckbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonDeckbuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokemon_deckbuilder/PokemonDeckbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemon_deckbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonDeckbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
